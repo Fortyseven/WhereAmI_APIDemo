b55191ccfd0e4476ad139a5562c4fd0db7e32be7
Fortyseven <fortyseven@gmail.com>
2016-01-25 00:22:38 -0500

    Not doing a POST request; removed unnecessary code

--- app/src/main/java/com/bytestemplar/whereami_apidemo/GeocodingRequest.java
+++ app/src/main/java/com/bytestemplar/whereami_apidemo/GeocodingRequest.java
@@ -11,19 +11,13 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
-import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
 
-/**
- * Created by Fortyseven on 2016-01-24.
- */
 public class GeocodingRequest
 {
-    private static final String    GM_API_KEY = "AIzaSyCMOf7KXK3BgVT_rXPiFuNpF8aSuyog5C0";
-    private static final MediaType JSON       = MediaType.parse( "application/json; charset=utf-8" );
+    private static final String GM_API_KEY = "AIzaSyCMOf7KXK3BgVT_rXPiFuNpF8aSuyog5C0";
 
     public interface GeocodingResponseHandler
     {
@@ -32,16 +26,13 @@ public class GeocodingRequest
 
     public static void makeRequest( Location loc, final GeocodingResponseHandler callback )
     {
-        OkHttpClient client       = new OkHttpClient();
-        RequestBody  request_body = RequestBody.create( JSON, "" );
+        OkHttpClient client = new OkHttpClient();
 
         String url = "https://maps.googleapis.com/maps/api/geocode/json?key=" + GM_API_KEY + "&latlng=" + loc.getLatitude() + "," + loc.getLongitude();
-        Log.i( "BT", "URL: " + url );
+
         Request request = new Request.Builder()
                 .url( url )
-                .post( request_body )
                 .build();
-
 
         client.newCall( request ).enqueue( new Callback()
         {
